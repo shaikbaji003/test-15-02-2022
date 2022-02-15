f2ace30d3fb58eb4545cd9d59121f7bbf3d78671
SHAIK BAJI <92388095+shaikbaji003@users.noreply.github.com>
2022-02-15 20:35:57 +0530

    Add files via upload

---
+++ JS-DSA-TEST-main (1)/JS-DSA-TEST-main/linearSearch/index.js
@@ -0,0 +1,54 @@
+// Implement Linear Search Algorithms that search a provided Array for the provided value.
+// The Linear Search Algorithms will either return a boolean, index, or value back.
+
+// const foods = ["pizza", "orange", "yogurt"];
+// linearSearchIndexOf(foods, "pizza") --> 0
+
+// linearSearchFind(foods, food => food === "pizza") --> "pizza"
+
+function linearSearchIndexOf(arr, val) {
+for(let i=0; i<arr.length; i++){
+  if(arr[i] === val){
+    return i;
+  }
+}
+  return -1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+const foods = ["pizza", "orange", "yogurt"];
+const people = [{ name: "Kevin", last: "Nguyen", born: 1995 }];
+
+describe("linearSearchIndexOf()", () => {
+  it("returns index of found item", () => {
+    assert.equal(linearSearchIndexOf(foods, "pizza"), 0);
+    assert.equal(linearSearchIndexOf(foods, "yogurt"), 2);
+  });
+  it("returns -1 if item not found", () => {
+    assert.equal(linearSearchIndexOf(foods, "laptop"), -1);
+  });
+});
+
+
+
+mocha.run();
---
+++ JS-DSA-TEST-main (1)/JS-DSA-TEST-main/reverseInt/index.js
@@ -0,0 +1,56 @@
+// Given an integer, return an integer with the digits
+// reversed.
+// --- Examples
+//   reverseInt(13) === 31
+//   reverseInt(404) === 404
+//   reverseInt(100) === 1
+//   reverseInt(-13) === -31
+//   reverseInt(-100) === -1
+
+function reverseInt(n) {
+  let reversed = n.toString().split('').reverse().join('');
+  reversed = parseInt(reversed);
+  if (n <0){
+    reversed = reversed * -1;
+
+  }
+  return reversed;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+mocha.setup("bdd");
+const { assert } = chai;
+
+describe("Integer Reversal", () => {
+  it("reverseInt() works on positive numbers", () => {
+    assert.equal(reverseInt(311), 113);
+    assert.equal(reverseInt(13), 31);
+    assert.equal(reverseInt(100), 1);
+    assert.equal(reverseInt(1408), 8041);
+  });
+
+  it("reverseInt() works on negative numbers numbers", () => {
+    assert.equal(reverseInt(-3), -3);
+    assert.equal(reverseInt(-13), -31);
+    assert.equal(reverseInt(-100), -1);
+    assert.equal(reverseInt(-1408), -8041);
+  });
+});
+
+mocha.run();
